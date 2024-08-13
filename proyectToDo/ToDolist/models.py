ba1c8d7564872b81c83ea3ca4628c04974ef876f
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-08-12 18:21:05 -0600

    Cambios en el django

--- proyectToDo/ToDolist/models.py
+++ proyectToDo/ToDolist/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, verbose_name='Title')
     description = models.TextField( verbose_name='Description',null=True)
-    Completed = models.BooleanField(default=False)
-    priority = models.IntegerField(default=0, verbose_name='Priority')
+    priority = models.IntegerField(
+        verbose_name='Priority',
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
 
     def __str__(self):
        fila ="Titulo: "+self.title + " Descripcion: "+self.description
